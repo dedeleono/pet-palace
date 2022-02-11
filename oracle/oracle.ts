427f4a08d7f2f7e32ff5c500164da7e515cd46de
GageBachik <loser@gagebachik.com>
2022-02-11 15:05:29 -0800

    working oracle

--- oracle/oracle.ts
+++ oracle/oracle.ts
@@ -11,7 +11,9 @@ import * as fs from "fs";
 async function main(args: any) {
   const keypair = "../lib/SEA1xkZzPCUJBb5mcNb6ts9VExNr2kYMit3T5poqr94.json";
 
-  const connection = new web3.Connection("https://api.mainnet-beta.solana.com");
+  const connection = new web3.Connection(
+    "https://crimson-withered-water.solana-mainnet.quiknode.pro/1fe9db637760863cd9720e3f325a1f6d6f15a5c9/"
+  );
 
   const wallet = web3.Keypair.fromSecretKey(
     new Uint8Array(JSON.parse(fs.readFileSync(keypair).toString()))
@@ -34,59 +36,10 @@ async function main(args: any) {
 
   const program = new Program(idl, petPalace.toString(), provider);
   // console.log("program got ran", program);
-  // default behavior new jollyranch each test
-
-  // const jollyranch = anchor.web3.Keypair.generate();
-  // switch to pda account for same jollyranch testing
-
-  // console.log("program", program);
-
-  // console.log("program", program.programId.toString());
-
-  // pda generation example
-  let [jollyranch, jollyBump] = await anchor.web3.PublicKey.findProgramAddress(
-    [Buffer.from("jolly_account")],
-    program.programId
-  );
-
-  // console.log("jollyranch", jollyranch.toBase58());
-  // console.log("jollyBump", jollyBump);
-
-  // use your own token here ex CHEESE
-  // devnet triton: 7RDibaGCRPSNBecU34AQPDioVYtgz1adYzPVaF4uryd9
-  // const spl_token = new anchor.web3.PublicKey(
-  //   "7RDibaGCRPSNBecU34AQPDioVYtgz1adYzPVaF4uryd9"
-  // );
-  const spl_token = new anchor.web3.PublicKey(
-    "8rDACnycUMGFvndX74ZM9sxjEbR3gUpVHDjDbL4qW6Zf"
-  );
-
-  const [recieverSplAccount, splBump] =
-    await anchor.web3.PublicKey.findProgramAddress(
-      [jollyranch.toBuffer()],
-      program.programId
-    );
-  // console.log("recieverSplAccount", recieverSplAccount.toBase58());
-  // console.log("splBump", splBump);
-
-  // console.log("wallet", wallet);
-  // console.log("wallet pulbic key", wallet.publicKey.toString());
-
-  let wallet_token_account = await Token.getAssociatedTokenAddress(
-    ASSOCIATED_TOKEN_PROGRAM_ID,
-    TOKEN_PROGRAM_ID,
-    spl_token,
-    wallet.publicKey
-  );
-  // console.log("wallet_token_account", wallet_token_account.toBase58());
-
-  const jollyAccount = await program.account.jollyRanch.fetch(
-    jollyranch.toString()
-  );
 
   console.log("oracle running");
   console.log("");
-  const a = program.provider.connection.onProgramAccountChange(
+  program.provider.connection.onProgramAccountChange(
     program.programId,
     async (programAccount) => {
       // console.log("programAccount", programAccount);
@@ -104,11 +57,12 @@ async function main(args: any) {
           await program.rpc.oracle(seed, {
             accounts: {
               authority: program.provider.wallet.publicKey,
-              breed: breed[0].publicKey,
+              breed: programAccount.accountId,
             },
           });
           console.log("breed succesfully parsed");
-        } catch {
+        } catch (e) {
+          console.log(e);
           console.log("parsing breed failed run the failsafe");
         }
       }
